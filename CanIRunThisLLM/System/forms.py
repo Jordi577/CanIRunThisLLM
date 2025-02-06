37998c2f0f956dc9e6b3a25b6d17dd63a50ef66f
Jordi <Jordi.buscaglia577@gmail.com>
2025-02-06 15:57:28 +0100

    Added a slider for the context window

--- CanIRunThisLLM/System/forms.py
+++ CanIRunThisLLM/System/forms.py
@@ -4,6 +4,7 @@ import json
 def read_llm_choices_mapping(json_file_path):
     with open(json_file_path, 'r', encoding='utf-8') as file:
         data = json.load(file)
+
     return data
 
 LLM_CHOICES_MAPPING = read_llm_choices_mapping("System/static/llm_map.json")
@@ -17,6 +18,19 @@ CONFIGURATION_MODE_CHOICES = [
     ('advanced', 'Advanced'),
 ]
 
+QUANTIZATION_CHOICES = [
+    ('fp32', 'FP32'),
+    ('fp16', 'FP16'),
+    ('q8', 'Q8'),
+    ('q7', 'Q7'),
+    ('q6', 'Q6'),
+    ('q5', 'Q5'),
+    ('q4', 'Q4'),
+    ('q3', 'Q3'),
+    ('q2', 'Q2'),
+    ('q1', 'Q1'),
+]
+
 class VRAMCalculationForm(forms.Form):
     configuration_mode = forms.ChoiceField(
         choices=CONFIGURATION_MODE_CHOICES,
@@ -24,70 +38,69 @@ class VRAMCalculationForm(forms.Form):
         initial='simple',
         help_text="Choose the configuration method."
     )
+    
     selected_llm = forms.ChoiceField(
         choices=LLM_CHOICES,
         required=False,
         help_text="Select a predefined model to prepopulate the fields."
     )
-    # Field for manually entering a predefined model (if not using the extra form)
+
     predefined_model_custom = forms.CharField(
         required=False,
         help_text="Or enter a predefined model manually."
     )
+    
     huggingface_model_path = forms.CharField(
         required=False,
         help_text="Enter a Hugging Face model path (e.g., 'meta-llama/Llama-2-7b-chat-hf')."
     )
+
     parameters_model = forms.FloatField(
         required=True,
         help_text="Enter the # of parameters in billions (e.g., 7.0 for 7B)"
     )
-    QUANTIZATION_CHOICES = [
-        ('fp32', 'FP32'),
-        ('fp16', 'FP16'),
-        ('q8', 'Q8'),
-        ('q7', 'Q7'),
-        ('q6', 'Q6'),
-        ('q5', 'Q5'),
-        ('q4', 'Q4'),
-        ('q3', 'Q3'),
-        ('q2', 'Q2'),
-        ('q1', 'Q1'),
-    ]
+
     quantization_level = forms.ChoiceField(
         choices=QUANTIZATION_CHOICES,
         required=True,
         help_text="Select the quantization level."
     )
-    # Advanced fields
+
     context_window = forms.IntegerField(
         required=False,
         help_text="Enter the context window size (e.g., 2048)"
     )
+
     cache_bit = forms.IntegerField(
         required=False,
         help_text="Enter the cache bit size (e.g., 16 or 8)"
     )
+
     num_attention_heads = forms.IntegerField(
         required=False,
         help_text="Enter the number of attention heads"
     )
+
     num_key_value_heads = forms.IntegerField(
         required=False,
         help_text="Enter the number of key-value heads"
     )
+
     hidden_size = forms.IntegerField(
         required=False,
         help_text="Enter the hidden size (e.g., 4096)"
     )
+
     num_hidden_layers = forms.IntegerField(
         required=False,
         help_text="Enter the number of hidden layers"
     )
+    
     ram = forms.FloatField(
         required=True,
         help_text="Enter your system's RAM in GB (optional if not using the .exe)."
     )
+
     gpu_vram = forms.FloatField(
         required=True,
         help_text="Enter your GPU VRAM in GB (optional if not using the .exe)."
@@ -107,7 +120,6 @@ class VRAMCalculationForm(forms.Form):
             self.fields["hidden_size"].initial = config["model_config"]["hidden_size"]
             self.fields["num_hidden_layers"].initial = config["model_config"]["num_hidden_layers"]
 
-
 class SystemInformation(forms.Form):
     system_ram = forms.IntegerField(
         required=True,
@@ -120,6 +132,7 @@ class SystemInformation(forms.Form):
         }),
         help_text="Enter your system's RAM in GB (optional if not using the .exe)."
     )
+
     system_vram = forms.FloatField(
         required=True,
         widget=forms.NumberInput(attrs={
@@ -132,14 +145,24 @@ class SystemInformation(forms.Form):
         help_text="Enter your GPU VRAM in GB (optional if not using the .exe)."
     )
 
+    context_window = forms.FloatField(
+        required=True,
+        widget=forms.NumberInput(attrs={
+            'type': 'range',
+            'min': '1',
+            'max': '8192',   
+            'step': '1',
+            'oninput': 'vramOutput.value = this.value'
+        }),
+        help_text="Enter your wished context windows."
+    )
+
 class HuggingfaceModelForm(forms.Form):
-    """
-    This form is used exclusively to accept a manually entered model.
-    """
     predefined_model_custom = forms.CharField(
         required=False,
         help_text="Enter a predefined model manually."
     )
+
     huggingface_model_path = forms.CharField(
         required=False,
         help_text="Enter a Hugging Face model path (e.g., 'meta-llama/Llama-2-7b-chat-hf')."
